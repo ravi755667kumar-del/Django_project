ab55f8a2822398c71b545881ffa834e0363c3017
Ravi kumar <ravi755667kumar@gmail.com>
2026-08-08 15:05:34 +0530

    add update for render deploy

--- tea/tea/settings.py
+++ tea/tea/settings.py
@@ -31,7 +31,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY", "fallback-unsafe-secret-key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 CSRF_TRUSTED_ORIGINS = [
     "https://anything-identified-wednesday-amp.trycloudflare.com",
 ]
