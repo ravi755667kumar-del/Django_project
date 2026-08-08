59e8da9b1ce4b86c4a195f3acd63c422d98f3671
Ravi kumar <ravi755667kumar@gmail.com>
2026-08-08 13:52:42 +0530

    Fix CSS loading in production with Whitenoise

--- tea/tea/settings.py
+++ tea/tea/settings.py
@@ -130,6 +130,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+WHITENOISE_USE_FINDERS = True
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
